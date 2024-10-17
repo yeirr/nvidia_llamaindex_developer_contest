1056b95478e75a1d3685f43e7edb6f901d557af2
yeirr <jaylyesk@gmail.com>
2024-10-17 17:41:22 +0800

    feat: add initial support for system message

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ llm = NVIDIA(
     base_url=f"http://localhost:{PORT}/v1",
     max_tokens=32,
 )
-prompt = "who are you? please elaborate in less then 100 words."
+system_message = "You are a helpful and honest assistant."
+message = "who are you? please elaborate in less then 100 words."
 
 # Sanity check.
 # chat_response = llm.complete(prompt)
@@ -31,11 +32,12 @@ prompt = "who are you? please elaborate in less then 100 words."
 async def main() -> None:
     # Set a default model.
     if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "facebook/opt-125m"
+        st.session_state["openai_model"] = MODEL_ID
 
     # Initialize chat history.
     if "messages" not in st.session_state:
         st.session_state.messages = []
+        st.session_state.messages.append({"role": "system", "content": system_message})
 
     # Display chat messages from history on app rerun.
     for message in st.session_state.messages:
@@ -43,17 +45,17 @@ async def main() -> None:
             st.markdown(message["content"])
 
     # Accept user input.
-    if prompt := st.chat_input("Message"):
+    if message := st.chat_input("Message"):
         # Add user message to chat history.
-        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.messages.append({"role": "user", "content": message})
 
         # Display user message in chat message container.
         with st.chat_message("user"):
-            st.markdown(prompt)
+            st.markdown(message)
 
         # Display assistant response in chat message container.
         with st.chat_message("assistant"):
-            chat_response = await llm.acomplete(prompt)
+            chat_response = await llm.acomplete(message)
             st.write(chat_response.text)
 
         # Write to history.
